862492ac007dd91cd8b6da5a25d84d0554da815d
Hugo <hfournier001@ensc.fr>
2019-04-25 22:51:23 +0200

    Version 0
    - création des cellules à l'échelle de 1 = 1 mètre
    - physique non appropriée du joueur
    - caméra virtuelle qui suit de loin le joueur

---
+++ Assets/Scripts/Deplacements.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacements : MonoBehaviour {
+
+
+    private Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //j'ai copié collé mon vieil exemple de roll-a-ball, on comprendra plus tard
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+
+}
